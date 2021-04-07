36987e950b21c17dbb1e9aa17da0944f8eeee846
xzk <1399350807@qq.com>
2021-04-07 11:42:02 +0800

    add requirements.txt

--- index_test.py
+++ index_test.py
@@ -1,12 +1,22 @@
 import torch
 import numpy as np
-t = torch.arange(0, 10).reshape(2, 5)
+t = torch.arange(0, 16).reshape(4, -1)
 print(t)
 
-indices = torch.from_numpy(np.array([2, 4]))
+# t1 = t[[1, 3, 1], [0, 1, 2]]
+# print(t1)
+# print(t.shape, t1.shape)
 
-t1 = torch.index_select(t, 1, indices)
-print(t1)
+mask = t > 7
+print(mask)
+print(mask.dtype)
 
-t1 = torch.gather(t, 0, indices)
+t1 = t[mask]
 print(t1)
+print(t1.dtype)
+
+# t1 = torch.index_select(t, 1, indices)
+# print(t1)
+#
+# t1 = torch.gather(t, 0, indices)
+# print(t1)
